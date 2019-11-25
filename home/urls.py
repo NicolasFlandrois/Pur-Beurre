e3350bee64f65fec4e0756c546a874f52a94952f
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-11-25 22:25:43 +0100

    Update - Contact (Info & URLS)
    Not entirely successful. The URLS doesn't redirect to the contact section of Homepage... So far it only opens homepage.

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('legal/', views.legal, name='home-legal'),
+    path('contact/', views.home, name='home-contact'),
     path('', views.home, name='home-home'),
 ]
