78c0ddd5f89ff4074ab3d83dbd355d86caef2f50
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-11-27 17:43:09 +0100

    Update UI - Base + Home Page
    Added icons, Pictures, Logo, Text (Colette & Remy description)

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'snacks.apps.SnacksConfig',
     'users.apps.UsersConfig',
     'home.apps.HomeConfig',
     'crispy_forms',
@@ -128,5 +129,5 @@ MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_REDIRECT_URL = 'home-home'
 LOGIN_URL = 'login'
