9b5a49bebe540ba3c4d3326d18eb88a1bd995efe
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-11-28 16:55:48 +0100

    Migration and Update
    Update allow to create superuser, by adding overwrite save() arguments with args* and kargs*

--- users/models.py
+++ users/models.py
@@ -5,14 +5,17 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='profile_default.jpg', upload_to='profile_pics')
-    # Add here more field (added on top of User's model) to appear in profile e.g. Bio, City, langage, etc
+    image = models.ImageField(default='profile_default.jpg',
+                              upload_to='profile_pics')
+
+    # Add here more field (added on top of User's model)
+    # to appear in profile e.g. Bio, City, langage, etc
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
