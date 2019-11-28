7d14e51a7079a3a91747ae6ceac128d6a243b9e8
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-11-28 12:04:15 +0100

    Updated Link go to Contact
    Used Get Parameter w/i Django URL/Jinja to go directly to the Anchor ID 'Contact' from Home Template...
    It works

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('legal/', views.legal, name='home-legal'),
-    path('contact/', views.home, name='home-contact'),
     path('', views.home, name='home-home'),
 ]
