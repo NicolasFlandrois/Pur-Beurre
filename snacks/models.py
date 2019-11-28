5e59a9c5216dbe5d18a75b37091c0104ce53e923
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-11-28 23:40:54 +0100

    Cleanned unused code

--- snacks/models.py
+++ snacks/models.py
@@ -5,20 +5,6 @@ from django.urls import reverse
 from PIL import Image
 
 
-# class Post(models.Model):
-#     """docstring for Post"""
-#     title = models.CharField(max_length=100)
-#     content = models.TextField()
-#     date_posted = models.DateTimeField(default=timezone.now)
-#     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.title
-
-#     def get_absolute_url(self):
-#         return reverse('post-detail', kwargs={'pk': self.pk})
-
-
 class Product(models.Model):
     """docstring for Products"""
     ean = models.CharField(max_length=13)
@@ -26,9 +12,7 @@ class Product(models.Model):
     category = models.CharField(max_length=50)
     image = models.ImageField(
         default='products_default.jpg', upload_to='products_pics')
-    nutriscore = models.CharField(max_length=100)
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # substitute = Column(Integer, ForeignKey('product.id'))   #???
+    nutriscore = models.CharField(max_length=1)
 
     def __str__(self):
         return self.name
@@ -47,25 +31,15 @@ class Product(models.Model):
             img.save(self.image.path)
 
 
-# class Category(models.Model):
-#     """docstring for Categories"""
-#     name = models.CharField(max_length=50)
-
-#     def __str__(self):
-#         return self.name
-
-#     def get_absolute_url(self):
-#         return reverse('category-detail', kwargs={'pk': self.pk})
-
-
-class Favourite(object):
+class Favourite(models.Model):
     """docstring for Favourite"""
     date_added = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user
+        return {"User": self.user, "Favourite": self.product,
+                "Date": self.date_added}
 
     def get_absolute_url(self):
         return reverse('favourite-detail', kwargs={'pk': self.pk})
