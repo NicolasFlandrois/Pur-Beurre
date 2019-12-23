fec6e14d8e4930d454e200dee55def457469c6aa
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-12-23 17:09:12 +0100

    Setting Unit Testing - Users App
    Not working so far

--- users/test/test_views.py
+++ users/test/test_views.py
@@ -5,4 +5,31 @@ from users.models import Profile
 
 
 class TestViews(TestCase):
-    """docstring for TestViews"""
+    """Class Testing Views in Users App"""
+
+    def setUp(self):
+        """Set Up variables used in this test"""
+        self.client = Client()
+        self.register_url = reverse('register')
+        self.profile_url = reverse('profile')
+
+        self.user_1 = User.objects.create_user(
+            pk=1,
+            username='Fav Models Unit Test 1',
+            email='boggusmail@boggusmail.net'
+        )
+
+    # Testing Function based views
+    def test_register_GET(self):
+        """Testing the register GET method's function"""
+        response = self.client.get(self.register_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    # def test_profile_GET(self):
+    #     """Testing the profile GET method's function"""
+    #     response = self.client.get(self.profile_url)
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'users/profile.html')
+    #     # This test doesn't work as it needs to pass LoginRequiredMixin, UserPassesTestMixin tests
+    #     # Without param variables matching thoses requisits, the server revert a 302 Error and response no templates
