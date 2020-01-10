736517167b116bd5b6ba8c8bb855fe5c952fb9ab
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-01-10 20:34:52 +0100

    Heroku Settings - allowing Host Heroku

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = True
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pur-beurre-nfl.herokuapp.com']
 
 
 # Application definition
