f8a8ed637d033d4bb48424263523ec24c6630aac
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-04-02 02:48:45 +0200

    Removed Email info
    Password have been modified

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'n.flandrois9github@gmail.com'
-EMAIL_HOST_PASSWORD = 'cemzdcjmlyvqxdch'
+EMAIL_HOST_USER = 'Your Email Here'
+EMAIL_HOST_PASSWORD = 'Your Password Here'
