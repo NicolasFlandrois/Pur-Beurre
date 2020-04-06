7ecc16554303e38f0af9702c5aa311495dc82424
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-04-06 20:51:25 +0200

    minor update - python Synthax

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nkyedh2a$c6&#0m%9i-e#v_(=x0pcx!xqn2vv=ujdh$y#8ox)v'
+SECRET_KEY = 'nkyedh2a$c6&#0m\%9i-e#v_(=x0pcx!xqn2vv=ujdh$y#8ox)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
